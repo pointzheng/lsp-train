13498bad226ed464757fdb0f5ffe64d3ec2ee490
zhengyy <zhengyy@calis.edu.cn>
2018-06-13 16:20:20 +0800

    调整菜单

--- src/config/index-menu.js
+++ src/config/index-menu.js
@@ -10,31 +10,8 @@ export const menuConfig = [{
         }]
     }]
 }, {
-    "openAPI-management": [{
-        menuName: "API注册管理",
-        menuList: [{
-            name: "API注册",
-            url: "./pages/API-registration.html"
-        }, {
-            name: "API变更",
-            url: "./pages/API-change.html"
-        }, {
-            name: "API启停",
-            url: "./pages/API-start-stop.html"
-        }]
-    }, {
-        menuName: "API文档管理",
-        menuList: [{
-            name: "文档发布",
-            url: "./pages/API-doc-publish.html"
-        }, {
-            name: "文档维护",
-            url: "./pages/API-doc-maintain.html"
-        }]
-    }]
-}, {
     "big-data-analysis": [{
-        menuName: "培训管理",
+        menuName: "框架培训",
         menuList: [{
             name: "article管理",
             url: "./pages/article.html"
@@ -45,4 +22,4 @@ export const menuConfig = [{
     }]
 }]
 
-export const menuTitleConfig = ["", "OpenAPI管理", "大数据分析"]
+export const menuTitleConfig = ["", "培训管理"]
