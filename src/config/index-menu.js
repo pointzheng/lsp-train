3ab96ec74184d452bfd78754e77ef5020e96b2a8
zhengyy <zhengyy@calis.edu.cn>
2018-06-14 10:45:01 +0800

    精简菜单

--- src/config/index-menu.js
+++ src/config/index-menu.js
@@ -1,22 +1,22 @@
 export const menuConfig = [{
-    "account": [{
-        menuName: "我的账户",
-        menuList: [{
-            name: "个人信息",
-            url: "./pages/my-job.html"
-        }]
+  "account": [{
+    menuName: "我的账户",
+    menuList: [{
+      name: "个人信息",
+      url: "./pages/my-job.html"
     }]
+  }]
 }, {
-    "big-data-analysis": [{
-        menuName: "框架培训",
-        menuList: [{
-            name: "article管理",
-            url: "./pages/article.html"
-        }, {
-            name: "租客映射模板管理",
-            url: "./pages/tenant-template.html"
-        }]
-    }]
+  "big-data-analysis": [{
+    menuName: "框架培训",
+    menuList: [{
+      name: "Article管理",
+      url: "./pages/article.html"
+    }/*, {
+      name: "租客映射模板管理",
+      url: "./pages/tenant-template.html"
+    }*/]
+  }]
 }]
 
 export const menuTitleConfig = ["", "培训管理"]
