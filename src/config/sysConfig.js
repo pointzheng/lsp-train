fa2f6ae4732479ce93727d63fcc3995617da49a9
zhengyy <zhengyy@calis.edu.cn>
2018-06-14 10:43:50 +0800

    精简，去除无用的配置

--- src/config/sysConfig.js
+++ src/config/sysConfig.js
@@ -1,39 +1,12 @@
 /**
  * 系统配置，包括：
- * (1) 服务器配置：预置1个测试服务器，一个正式服务器。
- * 其中，测试服务器，用于开发阶段的调试。正式服务器，用于实际部署。
- * (2) 是否允许登录
- * @author zhengyy
+ * (1) 服务器配置
  */
 
-
-// 测试服务器配置
-const confDev = {
-  SERVICE_BASE: "http://222.29.81.251:9130",
-  tenant: "l001736",
-  userId: sessionStorage.getItem("userId"),
-  currentAdmin: sessionStorage.getItem("username"),
-  token: sessionStorage.getItem("token")
-};
-
-// 正式服务器配置
+// Okapi服务器配置
 const confProd = {
-  SERVICE_BASE: "http://222.29.81.251:9130", // 服务器配置，待定
-  tenant: "l001736",
-  userId: sessionStorage.getItem("userId"),
-  currentAdmin: sessionStorage.getItem("username"),
-  token: sessionStorage.getItem("token")
+  serverBase:   "http://222.29.81.251:9130",
+  tenant:       "l001736"
 };
 
-const method2Code = {
-  GET: 200,
-  POST: 201,
-  PUT: 204,
-  DELETE: 204
-};
-
-const mode = "prod"; // 取值：dev和prod/specificationLib
-
-const anonyLogin = false; // 是否允许匿名登录（即：登录界面中，不验证用户名密码）
-
-export {confDev, confProd, method2Code, mode, anonyLogin};
+export {confProd};
--- src/util/util.js
+++ src/util/util.js
@@ -1,13 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import { Select, message } from 'antd';
+import { Select } from 'antd';
 import 'whatwg-fetch';
-import {confDev4SpecificationLib, confDev, confProd, method2Code, mode, anonyLogin} from '../config/sysConfig.js';
+import {confProd} from '../config/sysConfig.js';
+
 const Option = Select.Option;
+const method2Code = {
+  GET: 200,
+  POST: 201,
+  PUT: 204,
+  DELETE: 204
+};
 
 /**
  *
  * 工具函数模块，主要实现与业务无关的通用函数
- * @author kongxc, zhengyy
+ * @author zhengyy
  */
 const util = {
   trim(txt) {
@@ -15,9 +22,9 @@ const util = {
   },
 
   /**
-   * 获取页面参数
-   * @param  {[type]} name [description]
-   * @return {[type]}      具体的参数值，如果无值，返回null
+   * 获取请求地址URL中的参数
+   * @param  {[type]} name  参数名称
+   * @return {[type]}      具体的参数值。如果无值，返回null
    */
   getParameter(name) {
 		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
@@ -52,16 +59,14 @@ const util = {
   },
 
   /**
-   * 根据当前模式（开发或是生产模式），获取服务器配置信息：服务基地址、馆名称（英文和中文）、用户名等
-   * @param config 各个参数配置，JS Object类型。当前作为备用，尚未启用。
+   * 获取服务器配置信息：服务基地址、馆名称（英文和中文）、用户名等
    */
-  getServerConfig(config) {
-    return confProd;
-  },
-
-  // 是否允许匿名登录
-  isAnonyLogin() {
-    return anonyLogin;
+  getServerConfig() {
+    return Object.assign({}, confProd, {
+      userId: sessionStorage.getItem("userId"),
+      currentAdmin: sessionStorage.getItem("username"),
+      token: sessionStorage.getItem("token")
+    })
   }
 }
 
