91e78daeb63e542fd31aed13e932dba1bec2197a
zhengyy <zhengyy@calis.edu.cn>
2018-06-15 09:36:11 +0800

    去除无用注释

--- webpack.config.js
+++ webpack.config.js
@@ -12,15 +12,7 @@ var happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
 var nodeExternals = require("webpack-node-externals");
 var loaders = baseConf.loaders.concat({
     test: /\.(js|jsx)$/,
-    // exclude: /node_modules/,
     exclude: path.resolve(__dirname, "node_modules"),
-
-    // TODO：保留下面注释语句，待后续对打包性能进行完善
-    // include: resolve('src'),
-    // exclude: resolve('node_modules'),
-    // target: 'node', // in order to ignore built-in modules like path, fs, etc.
-    // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-    // loader: 'happypack/loader?id=happybabel'
     loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0&cacheDirectory=true'
 });
 
@@ -38,27 +30,14 @@ var config = {
     },
     resolve: {
         alias: {
-            // 'react': path.join(__dirname, 'node_modules', 'react','dist','react.min.js'),
-            // 'react-dom': path.join(__dirname, 'node_modules', 'react-dom','dist','react-dom.min.js'),
-            // 'jquery': path.join(__dirname, 'node_modules', 'jquery','dist','jquery.min.js'),
-            // 'jquery-custom': path.join(__dirname, 'src', 'plugins','jquery-custom','jquery-custom.js'),
-            // 'react-bootstrap': path.join(__dirname, 'node_modules', 'react-bootstrap','dist','react-bootstrap.min.js'),
-            // 'babel-polyfill': path.join(__dirname, 'node_modules', 'babel-polyfill','dist','polyfill.min.js'),
-            'bfd': path.join(__dirname, 'node_modules', 'bfd-ui', 'lib')
+          'bfd': path.join(__dirname, 'node_modules', 'bfd-ui', 'lib')
         }
     },
-    plugins: htmlPlugins/*.concat(new HappyPack({
-     id: 'happybabel',
-     loaders: ['babel-loader'],
-     threadPool: happyThreadPool,
-     cache: true,
-     verbose: true
-     }))*/,
-    babel:{
+    plugins: htmlPlugins,
+    babel: {
         plugins:[["import", [{ "libraryName": "antd", "style": true }]]]
     },
     devServer: {
-        //devtool: '#source-map',
         contentBase: '/',
         host: '0.0.0.0',
         port: 7000,
